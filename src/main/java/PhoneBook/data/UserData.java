510bc87c02a8db79888b422827545ba6e28d89fb
Galinatroshina <galinatroshina@gmail.com>
2025-02-13 13:22:08 +0100

    first commit

---
+++ src/main/java/PhoneBook/data/UserData.java
@@ -0,0 +1,7 @@
+package PhoneBook.data;
+
+public class UserData {
+    public static final String VALID_EMAIL = "galchibu1@gmail.com";
+    public static final String RANDOM_EMAIL = "galchibu1"+ System.currentTimeMillis() +"@gmail.com";
+    public static final String VALID_PASSWORD = "Qwerty1@";
+}
